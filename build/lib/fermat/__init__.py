19a86911df9bb05c1ca76e827531e315d26a8659
nicocheh <nicocheh@hotmail.com>
2021-06-14 20:51:13 -0300

    actualizo

--- build/lib/fermat/__init__.py
+++
@@ -1,3 +0,0 @@
-from fermat.Fermat import Fermat
-
-__all__ = ['path_methods', 'Fermat']
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='fermat',
-      version='0.0.2',
-      description='library to compute fermat distance',
-      author='Facundo Sapienza',
-      author_email='f.sapienza@aristas.com.ar',
-      packages=find_packages(),
-    )
